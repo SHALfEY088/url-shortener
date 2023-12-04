e68fccc7f68b1d250056595ab91b308820837ef8
SHALfEY088 <shalfey088@gmail.com>
2023-12-05 02:30:26 +0300

    added logger

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -3,6 +3,15 @@ package main
 import (
 	"fmt"
 	"github.com/SHALfEY088/url-shortener/internal/config"
+	"github.com/SHALfEY088/url-shortener/internal/lib/logger/handlers/slogpretty"
+	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
@@ -10,7 +19,14 @@ func main() {
 
 	fmt.Println(cfg)
 
-	// TODO: init logger: slog
+	log := setupLogger(cfg.Env)
+
+	log.Info(
+		"starting url-shortener",
+		slog.String("env", cfg.Env),
+		slog.String("version", "0.0.1"),
+	)
+	log.Debug("debug messages are enabled")
 
 	// TODO: init storage: sqlite
 
@@ -18,4 +34,39 @@ func main() {
 
 	// TODO: run server:
 
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = setupPrettySlog()
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	default: // If env config is invalid, set prod settings by default due to security
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
+}
+
+func setupPrettySlog() *slog.Logger {
+	opts := slogpretty.PrettyHandlerOptions{
+		SlogOpts: &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		},
+	}
+
+	handler := opts.NewPrettyHandler(os.Stdout)
+
+	return slog.New(handler)
 }
